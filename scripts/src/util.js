2b239fea1d5955ab4de7f6f1cfdde537dd2dacf5
munkh99 <munkh99ro@gmail.com>
2025-01-14 14:59:39 +0100

    update filter

--- scripts/src/util.js
+++ scripts/src/util.js
@@ -65,14 +65,18 @@ export function createDatasetFilters(filters) {
         }
 
         if (filters.duration_facet) {
-            if (dataset.data_type_facet) {
-                console.log("filter duration_facet")
-                console.log(filters.duration_facet)
-                console.log(dataset.duration_facet)
-                conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
-            } else {
-                console.log("Invalid data_type_facet:", dataset.data_type_facet); // Handle invalid or null data_type_facet
-            }
+            // if (dataset.data_type_facet) {
+            //     console.log("filter duration_facet")
+            //     console.log(filters.duration_facet)
+            //     console.log(dataset.duration_facet)
+            //     conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
+            // } else {
+            //     console.log("Invalid data_type_facet:", dataset.data_type_facet); // Handle invalid or null data_type_facet
+            // }
+            console.log("filter duration_facet-15:00")
+            console.log(filters.duration_facet)
+            console.log(dataset.duration_facet)
+            // conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
         }
 
 
