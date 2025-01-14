25aeb5eed567303cd469913be369c71ecc9c8a30
munkh99 <munkh99ro@gmail.com>
2025-01-14 15:05:31 +0100

    update filter - accounting nan

--- scripts/src/util.js
+++ scripts/src/util.js
@@ -58,25 +58,19 @@ export function createDatasetFilters(filters) {
         }
 
         if (filters.data_type_facet) {
-            console.log("filter data_type_facet")
-            console.log(slugify(dataset.data_type_facet))
-            console.log(filters.data_type_facet)
-            conditions.push(dataset.data_type_facet && slugify(dataset.data_type_facet).indexOf(filters.data_type_facet) !== -1)
+            if(dataset.data_type_facet){
+                console.log("filter data_type_facet-15:05")
+                console.log(slugify(dataset.data_type_facet))
+                console.log(filters.data_type_facet)
+                conditions.push(dataset.data_type_facet && slugify(dataset.data_type_facet).indexOf(filters.data_type_facet) !== -1)}
+            else{
+                console.log("Invalid data_type_facet:", dataset.data_type_facet); // Handle invalid or null data_type_facet
+                conditions.push("");
+            }
         }
 
         if (filters.duration_facet) {
-            // if (dataset.data_type_facet) {
-            //     console.log("filter duration_facet")
-            //     console.log(filters.duration_facet)
-            //     console.log(dataset.duration_facet)
-            //     conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
-            // } else {
-            //     console.log("Invalid data_type_facet:", dataset.data_type_facet); // Handle invalid or null data_type_facet
-            // }
-            console.log("filter duration_facet-15:00")
-            console.log(filters.duration_facet)
-            console.log(dataset.duration_facet)
-            // conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
+            conditions.push(dataset.duration_facet && slugify(dataset.duration_facet) === (filters.duration_facet))
         }
 
 
